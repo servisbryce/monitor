684b4b167358d5a6f854dee14ee519e98469b122
servisbryce <bryce@bryceserv.is>
2025-05-06 21:22:54 -0500

    Added latency path.

--- src/server/errors.py
+++ src/server/errors.py
@@ -1,12 +1,11 @@
 from flask import jsonify
 
-# Define a function to alert the client that the request didn't succeed and the request is malformed.
-def malformed_request():
-    
-    # Alert the client that the request didn't succeed and the request is malformed.
+# Define a function to alert the client that the request didn't succeed because the timestamp drift is negative.
+def misconfigured_clock():
+
+    # Alert the client that the request didn't succeed and the request is therefore invalid.
     return jsonify({
 
-        "status": "failure",
-        "message": "malformed request"
+        "message": "The timestamp is in the future. Please verify your clock synchronization.",
 
     }), 400
--- src/server/main.py
+++ src/server/main.py
@@ -1,5 +1,7 @@
 # Import the Flask module and a few other helpers.
 from flask import Flask, jsonify, request
+from errors import misconfigured_clock
+import time
 
 # Define the Flask application.
 app = Flask(__name__)
@@ -20,17 +22,25 @@ def root():
 # Define a route for a client to measure latency between the client and server. This route shall not be protected as it doesn't reveal any sensitive information.
 @app.route("/latency", methods=["POST"])
 def latency():
-
+    # To reduce latency inaccuracies, we're going to measure the timestamp as soon as the request is received.
+    current_timestamp = time.time()
+    
+    # Read the body of the request.
     body = request.get_json()
-    # Ensure that the request actually has a body.
-    if body:
-        print("hi")
 
-    else:
-        # Alert the client that the request didn't succeed and the request is malformed.
-        return jsonify({
+    # Retrieve the timestamp from the request and subtract it from the current timestamp.
+    past_timestamp = body.get("timestamp")
+    drift_timestamp = current_timestamp - past_timestamp
 
-            "status": "failure",
-            "message": "malformed request"
+    # Verify that the timestamp makes sense.
+    if (drift_timestamp < 0):
 
-        }), 400
+        # If we've branched here, then the timestamp is in the future. Our or the client's clock is wrong.
+        return misconfigured_clock()
+
+    # Otherwise, the timestamp is valid. Our drift timestamp is thus our latency.
+    return jsonify({
+
+        "latency": drift_timestamp,
+
+    }), 200
