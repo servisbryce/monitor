e40b456ceda752e555a8eb4b4b98aa0ec088f6bc
servisbryce <bryce@bryceserv.is>
2025-05-10 20:03:13 -0500

    Moved the authentication middleware into its own function.

--- src/server/main.py
+++ src/server/main.py
@@ -17,6 +17,23 @@ application_secrets = [
 
 ]
 
+# Authentication middleware.
+def authenticate(body):
+    
+    # Attempt to retrieve the token and compare it to the known list of valid tokens.
+    try:
+
+        if body["token"] not in application_secrets:
+            raise ValueError("Invalid token.")
+        
+    # If the token is present but invalid, we'll declare it.
+    except ValueError as error_message:
+        abort(403, error_message)
+
+    # Otherwise, there isn't a token present in the body, so we'll throw the relevant error code and abort.
+    except:
+        abort(401, "There is no token present.")
+
 # An endpoint to report latency to the server.
 @monitor.route("/api/v1/report_latency", methods=["POST"])
 def report_latency():
@@ -25,23 +42,7 @@ def report_latency():
     body = request.get_json()
     
     # Catch any authorization errors right away before they make it into the rest of the program.
-    try: 
-
-        # Verify if the token in the body is valid.
-        if body["token"] not in application_secrets:
-            raise ValueError("Invalid token.")
-
-    # If the token is present but invalid, then we'll declare it.
-    except ValueError as error_message:
-
-        # Declare the error to the client.
-        abort(403, error_message)
-
-    # Otherwise, there isn't a token present and we're going to report the proper error code.
-    except:
-        
-        # If the token is not present, abort with a unauthorized error.
-        abort(401, "There is no token present.")
+    authenticate(body)
 
     # If we've reached this part of the function, the used has authorized themselves. We'll try to grab the timestamp.
     requested_at = None
@@ -81,10 +82,3 @@ def report_latency():
         "network_latency": network_latency
 
     })
-
-# An endpoint to report network interfaces.
-@monitor.route("/api/v1/report_network_interfaces", method=["POST"])
-def report_network_interfaces():
-
-    # Retrieve request body which should be a JSON object.
-    body = request.get_json()
